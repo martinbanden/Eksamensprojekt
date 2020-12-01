1730520e67db35e34bfdf255ca3e9aa693eb6984
martinbanden <martinbanden@gmail.com>
2020-12-01 13:50:05 +0100

    blah

--- execution.js
+++ execution.js
@@ -1,2 +1,7 @@
 'use strict'
 import {$, createCookie, readCookie, eraseCookie} from "http://localhost/webdev/projects/1. semester eksamen/modules/library.js";
+/*http://localhost/webdev/projects/1. semester eksamen angiv stien på jeres egen localhost for at afprøve*/
+
+
+
+var rbw = window.addEventListener('load', init); 
