b61e5e105e8c82b89802cfa22b77e09f297e9d9f
Khavushka <khavushka@gmail.com>
2020-12-01 09:56:57 +0100

    select/option html

--- modules/functions.js
+++ modules/functions.js
@@ -1,3 +1,4 @@
 'use strict'
 import {$, createCookie, readCookie, eraseCookie} from "http://localhost/webdev/projects/1. semester eksamen/modules/library.js";
 /*http://localhost/webdev/projects/1. semester eksamen angiv stien på jeres egen localhost for at afprøve*/
+
