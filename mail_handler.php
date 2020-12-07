2773f64b91492f5aca7c354acabb5204d9a7de71
Khavushka <khavushka@gmail.com>
2020-12-07 14:04:50 +0100

    Forsæt fra n1/n2/n3 i PhP
    Tilføje radio til andre HTML button

--- mail_handler.php
+++ mail_handler.php
@@ -3,16 +3,28 @@
 
 $navn = $_POST['navn'];
 $tlf = $_POST['tlf'];
+$tekst = $_POST['tekst'];
+$n1 = $_POST['n1'];
+$n2 = $_POST['n2'];
+$n3 =$_POST['n3'];
 
-$to = "khavushka@gmail.com"; //hvor brevet skal sendes
+$to = "eksempel@gmail.com"; //hvor brevet skal sendes
 $subject = "Fejlmelding"; //email-emne
-$body = "text "  . $name  ."\t"  . $tlf ; // form af teksten i meddelelsen til afsendelse
+$body =  $tekst; // form af teksten i meddelelsen til afsendelse
 
 // mail ($to, $subject, $body); //samler alle de indtastede data og sender dem til adressatens
 
-echo "Besked ...." . $body . $beskrivelse;
+echo "Beskeden sendt fra" ."\t" .$navn ."\t" . $body . "\t" . $n1 . "\t" . $n2 . "\t" . $n3;
+echo '<br>';
+echo htmlspecialchars($_POST['tlf']); 
+echo '<br>';
+// echo htmlspecialchars($_POST['n1']); 
+// echo '<br>';
+// echo htmlspecialchars($_POST['n2']); 
+// echo '<br>';
+// echo htmlspecialchars($_POST['n3']); 
 
-echo $_POST['navn']+$_POST['tlf']+$_POST['tekst']+$_POST['n1']+$_POST['n2']+$_POST['n3'];
+// echo $_POST['navn']+$_POST['tlf']+$_POST['tekst']+$_POST['n1']+$_POST['n2']+$_POST['n3'];
 
 // echo htmlspecialchars($_POST['navn']); 
 // echo  '<br>';
