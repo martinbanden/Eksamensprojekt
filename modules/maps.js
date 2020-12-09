15c0067b7deff32cd2f7657804c115203ddc8fa1
martinbanden <martinbanden@gmail.com>
2020-12-09 09:13:18 +0100

    fejl rettet

--- modules/maps.js
+++ modules/maps.js
@@ -4,7 +4,7 @@ const $ = function(foo) {
     return document.getElementById(foo);
 }
 function initMap() {
-$('map').src = "https://open.mapquestapi.com/staticmap/v5/map?locations=Tønder,dk&size=600,400@2x&key=KEY"
+$('map').src = "https://open.mapquestapi.com/staticmap/v5/map?locations=Tønder,dk&size=600,400@2x&key=3ZDz50uvowZWQbX1k3NprLeq47XcdtCl"
 
 
 /*http://maps.google.com/staticmap?center=tonder,dk&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7c54.931535,8.864084?key=AIzaSyDMaWoPntPbFyITyMKXcEXuIMKi9JUasbg*/
